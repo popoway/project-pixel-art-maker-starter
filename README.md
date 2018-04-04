2394ca50ed860dc8baf3d21a7a762fe77a15a7c5
popoway <31576265+popoway@users.noreply.github.com>
2018-04-03 22:44:18 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # Pixel Art Maker Project
 
+Build a single-page web app that allows users to draw pixel art on a customizable canvas! For example:
+![Screenshot of pixel_art](https://github.com/popoway/project-pixel-art-maker-starter/raw/master/preview.png)
+
 ## Table of Contents
 
+* [Demo](#demo)
+* [Requirements](#requirements)
 * [Instructions](#instructions)
-* [Contributing](#contributing)
+
+## Demo
+
+https://popoway.github.io/project-pixel-art-maker-starter/index.html
+
+## Requirements
+
+Your users should be able to:
+
+1. Dynamically set the size of the table as an N by M grid.
+2. Choose a color.
+3. Click a cell in the grid to fill that cell with the chosen color.
+
+Your primary task is to implement the `makeGrid()` function, that dynamically creates a grid that the user can interact with.
+
+In this project, you'll use your front-end programming skills from __JavaScript Foundations__ and __Intro to jQuery__. Although not a requirement, you are encouraged to use __jQuery__ to manipulate the DOM for this project.
 
 ## Instructions
 
@@ -11,6 +31,4 @@ To get started, open `designs.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+You can download the starter code through: [The Pixel Art Maker repository on GitHub](https://github.com/udacity/project-pixel-art-maker-starter)
--- designs.js
+++ designs.js
@@ -1,10 +1,52 @@
 // Select color input
+var foreCanvasColor = "#000000";
+$("#colorPicker").on("change",function(){
+  foreCanvasColor = $("#colorPicker").val();
+  console.log("foreCanvasColor = " + foreCanvasColor);
+});
 // Select size input
+var canvasHeight = 1 , canvasWidth = 1;
+$("#inputHeight").on("change",function(){
+  canvasHeight = $("#inputHeight").val();
+  console.log("canvasHeight = " + canvasHeight);
+});
+$("#inputWidth").on("change",function(){
+  canvasWidth = $("#inputWidth").val();
+  console.log("canvasWidth = " + canvasWidth);
+});
+// Listen to the click on pixels and change color subsequently
+// use event delegation
+$("table").on("mousedown","td",function(evt){
+  if ($(evt.target).attr("style")){
+    // if exists then remove (to white)
+    $(evt.target).removeAttr("style");
+    console.log("Pixel toggled to white");
+  }
+  else {
+    $(evt.target).css("background-color", foreCanvasColor);
+    console.log("Pixel toggled to " + foreCanvasColor);
+
+  }
+});
+
 
 // When size is submitted by the user, call makeGrid()
+$("#inputSubmit").on("click",makeGrid);
+function makeGrid(evt) {
+  // Your code goes here!
+  evt.preventDefault(); // instructs the browser not to perform the default action
 
-function makeGrid() {
+  // 1. remove existing canvas
+  $( "tr" ).remove();
+  $( "td" ).remove();
+  console.log("Old canvas removed");
 
-// Your code goes here!
-
+  // 2. build a new canvas with specific size
+  for (var i = 1; i <= canvasHeight; i++){
+    $("#pixelCanvas").append('<tr></tr>'); // add rows
+  }
+  for (var j = 1; j <= canvasWidth; j++){
+    $("#pixelCanvas").children().append('<td></td>'); // add columns
+  }
+  console.log("Build a " + canvasWidth + " x " + canvasHeight + " canvas");
 }
